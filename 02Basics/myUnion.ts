15429099261d7e5f6b42bba623b20d15bb2941ea
bchaindeveloper <bcdev3@protonmail.com>
2022-12-28 17:09:02 -0500

    Union

---
+++ 02Basics/myUnion.ts
@@ -0,0 +1,38 @@
+let score: number | string = 33
+score = 44
+score = "55"
+
+type User = {
+    name: string;
+    id: number
+}
+
+type Admin = {
+    username: string;
+    id: number
+}
+
+let Billy: User | Admin = {name: "Billy", id: 333}
+
+Billy = {username: "hc", id: 334}
+
+getDbId(3)
+getDbId("3")
+
+function getDbId(id: number | string){
+    // making some API calls
+    // console.log(`DB id is: ${id}`);
+    if (typeof id === "string") {
+        id.toLowerCase()
+    }
+}
+
+// array
+
+const data: number[] = [1, 2, 3]
+const data2: string[] = ["1", "2", '3']
+const data3: (string | number)[] = ["1", "2", 3]
+
+let seatAllotment: "aisle" | "middle" | "window"
+
+seatAllotment = "aisle"
