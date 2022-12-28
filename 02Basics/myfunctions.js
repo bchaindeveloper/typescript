46886abd0ee90335fabea342ec221097122527b5
bchaindeveloper <bcdev3@protonmail.com>
2022-12-28 12:49:31 -0500

    02Basics

---
+++ 02Basics/myfunctions.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getUpper('four');
+signUpUser('Billy', 'Billy@popcorn.com', true);
+loginUser('Hale', 'hale@hale.com');
--- 02Basics/myfunctions.ts
+++ 02Basics/myfunctions.ts
@@ -1,5 +1,18 @@
-function addTwo(num){
+function addTwo(num: number){
     return num + 2
 }
 
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
 addTwo(5)
+getUpper('four')
+signUpUser('Billy', 'Billy@popcorn.com', true)
+loginUser('Hale', 'hale@hale.com')
+
+export {}
