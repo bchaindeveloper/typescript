79c6f4f01a0a231a5c4008d9cccc06592ccbc378
bchaindeveloper <bcdev3@protonmail.com>
2022-12-28 15:05:37 -0500

    myArray

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,15 +1,14 @@
 "use strict";
+// const User = {
+//     name: "Billy",
+//     email: "billy@pop.com",
+//     isActive: true
+// }
 exports.__esModule = true;
-var User = {
-    name: "Billy",
-    email: "billy@pop.com",
-    isActive: true
+var myUser = {
+    _id: '1234',
+    name: "h",
+    email: "h@history.com",
+    isActive: false
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-}
-var newUser = { name: "Billy", isPaid: false, email: "billy@pop.com" };
-createUser(newUser);
-function createCourse() {
-    return { name: "reactjs", price: 250 };
-}
+myUser.email = "h@gmail.com";
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -1,20 +1,64 @@
-const User = {
-    name: "Billy",
-    email: "billy@pop.com",
-    isActive: true
+// const User = {
+//     name: "Billy",
+//     email: "billy@pop.com",
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}){
+
+// }
+
+// let newUser = {name: "Billy", isPaid: false, email: "billy@pop.com"}
+
+// createUser(newUser)
+
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactjs", price: 250}
+// }
+
+// type User = {
+//     name: string,
+//     email: string,
+//     isActive: bool
+// }
+// // This allows you to declare with bool instad of boolean
+// type bool = boolean
+
+// function createUser(user: User): User {
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
+type User = {
+    readonly _id: string
+    name: string,
+    email: string,
+    isActive: boolean,
+    credcardDetails?: number
 }
 
-function createUser({name: string, isPaid: boolean}){
-
+let myUser: User = {
+    _id: '1234',
+    name: "h",
+    email: "h@history.com",
+    isActive: false
 }
 
-let newUser = {name: "Billy", isPaid: false, email: "billy@pop.com"}
-
-createUser(newUser)
-
-
-function createCourse():{name: string, price: number}{
-    return {name: "reactjs", price: 250}
+type cardNumber = {
+    cardnumber: string
 }
+
+type cardDate = {
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+myUser.email = "h@gmail.com"
+// myUser._id = "asa"
 
 export {}
