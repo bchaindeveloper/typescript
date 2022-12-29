e15c56275e2d521be09294d52ea45fd479184c73
bchaindeveloper <bcdev3@protonmail.com>
2022-12-29 10:39:22 -0500

    Get Set

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,2 +1,9 @@
-"use strict";
-console.log("typescript is here");
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+
+var Billy = new User("Billy@bob.com", "Billy");
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,1 +1,35 @@
-console.log("typescript is here");
+class User {
+    // unmarked is public 
+    // public email: string
+    // name: string
+    private _courseCount = 1
+
+    readonly city: string = "Kings"
+    constructor(
+        public email: string, 
+        public name: string
+        ){
+    }
+
+    private deleteToken(){
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+const Billy = new User("Billy@bob.com", "Billy")
+
+// Billy.city = "Kings";
